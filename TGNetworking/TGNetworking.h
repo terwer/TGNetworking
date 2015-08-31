10f18d3433a434f697ab462c2e2ebc60cd5c7ebc
Terwer Green <cbgtyw@gmail.com>
2015-08-31 22:02:09 +0800

    Create project

---
+++ TGNetworking/TGNetworking.h
@@ -0,0 +1,19 @@
+//
+//  TGNetworking.h
+//  TGNetworking
+//
+//  Created by Terwer Green on 15/8/31.
+//  Copyright (c) 2015年 Terwer Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TGNetworking.
+FOUNDATION_EXPORT double TGNetworkingVersionNumber;
+
+//! Project version string for TGNetworking.
+FOUNDATION_EXPORT const unsigned char TGNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TGNetworking/PublicHeader.h>
+
+
